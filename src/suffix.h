7d8f535462899824a75cc3b1c048796d3f4e6d2f
mathbozzi <matheusabreubozzi@gmail.com>
2022-02-23 21:39:36 -0300

    in src

--- src/suffix.h
+++ src/suffix.h
@@ -4,26 +4,26 @@
 #include "str.h"
 
 typedef struct {
-    String *s;
-    int index;
+	String *s;
+	int index;
 } Suffix;
 
-Suffix* create_suffix(String *s, int index);
+Suffix *create_suffix(String *s, int index);
 
 void destroy_suffix(Suffix *suf);
 
 void print_suffix(Suffix *suf);
 
-Suffix** create_suf_array(String *text, int N);
+Suffix **create_suf_array(String *text, int N);
 
-void destroy_suf_array(Suffix* *a, int N);
+void destroy_suf_array(Suffix **a, int N);
 
-void print_suf_array(Suffix* *a, int N);
+void print_suf_array(Suffix **a, int N);
 
 // Use uma (ou mais) funcoes deste tipo para ordenar
 // o arry de sufixos usando o qsort e outro metodo de sua escolha
-void sort_suf_array(Suffix* *a, int N);
+void sort_suf_array(Suffix **a, int N);
 
-int rank(Suffix* *a, int N, String *query);
+int rank(Suffix **a, int N, String *query);
 
 #endif
